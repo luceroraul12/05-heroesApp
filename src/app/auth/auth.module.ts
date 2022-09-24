cbbae9d001e7b24f5b5af7473a733389d753353c
Lucero Raul <luceroraul12@gmail.com>
2022-09-24 01:44:32 -0300

    add admin module por lazyload

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-
-
+import { LoginComponent } from './pages/login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegistroComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, RegistroComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
