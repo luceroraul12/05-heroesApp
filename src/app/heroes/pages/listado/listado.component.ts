4a647111806cc578789fe313ee694362ba98d979
Lucero Raul <luceroraul12@gmail.com>
2022-09-25 17:52:48 -0300

    fix interface

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ListadoComponent implements OnInit {
+  public heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
   }
-
 }
