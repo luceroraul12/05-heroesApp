761b64ee093160250ffeaffa14e3b6cb499082ae
Lucero Raul <luceroraul12@gmail.com>
2022-09-25 17:31:01 -0300

    gen heroes service

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroesService {
+  constructor(private http: HttpClient) {}
+
+  hetHeroes() {
+    return this.http.get('http://localhost:3000/heroes');
+  }
+}
