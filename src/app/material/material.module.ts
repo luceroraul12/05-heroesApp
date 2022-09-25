a267445c9aee19ed763fb617300713dad2491132
Lucero Raul <luceroraul12@gmail.com>
2022-09-25 10:44:42 -0300

    export mat sidenav module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,5 +6,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 @NgModule({
   declarations: [],
   imports: [CommonModule, MatSidenavModule],
+  exports: [MatSidenavModule],
 })
 export class MaterialModule {}
