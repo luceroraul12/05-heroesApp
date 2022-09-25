f151df4cce16f0bc1201711d006b9106d3a2bc98
Lucero Raul <luceroraul12@gmail.com>
2022-09-25 19:19:13 -0300

    add ActivatedRoute

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, CanActivate, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.router.params.subscribe(({ id }) => console.log(id));
   }
-
 }
--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.router.params.subscribe(({ id }) => console.log(id));
   }
-
 }
