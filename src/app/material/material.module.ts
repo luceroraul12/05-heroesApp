5f919b39ee42367ed0b28a1f4a2fb2615043672c
Lucero Raul <luceroraul12@gmail.com>
2022-09-25 10:25:16 -0300

    add mat sidenav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatSidenavModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
