6d290590c47bda8dae72b6e77d5ac60d96675af6
Lucero Raul <luceroraul12@gmail.com>
2022-09-26 10:00:58 -0300

    add heroe template

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [],
+  styles: [`
+      img {
+        width: 100%;
+        border-radius: 20px;
+      }
+  `],
 })
 export class HeroeComponent implements OnInit {
-  constructor(private router: ActivatedRoute) {}
+  constructor(
+    private router: ActivatedRoute,
+    private heroesService: HeroesService
+  ) {}
+
+  @Input() heroe!: Heroe;
 
   ngOnInit(): void {
-    this.router.params.subscribe(({ id }) => console.log(id));
+    this.router.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroePorId(id)),
+        tap(console.log))
+      .subscribe((heroe: Heroe[]) => this.heroe = heroe[0]);
   }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
-    MatCardModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
 })
 export class MaterialModule {}
