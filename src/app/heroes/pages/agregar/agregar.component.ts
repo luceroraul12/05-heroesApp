db2db04951fc795a182a7f6387d9b1e7eb393461
Lucero Raul <luceroraul12@gmail.com>
2022-09-27 10:49:07 -0300

    add mat select module

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, CanActivate, RouterLinkActive } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -7,6 +8,27 @@ import { ActivatedRoute, CanActivate, RouterLinkActive } from '@angular/router';
   styles: [],
 })
 export class AgregarComponent implements OnInit {
+
+  public creadores =  [
+    {
+      id: "DC Comics",
+      desc: "DC - Comics"
+    },
+    {
+      id: "Marvel Comics",
+      desc: "Marvel - Comics"
+    }
+  ]
+
+  public heroe: Heroe = {
+      superhero: "",
+      alter_ego: "",
+      characters: "",
+      first_appearance: "",
+      publisher: Publisher.DCComics,
+      alt_img: ""
+  };
+
   constructor(private router: ActivatedRoute) {}
 
   ngOnInit(): void {
