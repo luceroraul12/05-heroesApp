4c8f28587d5b02ca804a1899954e2e11e13433d6
Lucero Raul <luceroraul12@gmail.com>
2022-10-09 02:21:46 -0300

    implementacion de canActivate

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate, CanLoad {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.auth ? true : false;
   }
   canLoad(
     route: Route,
